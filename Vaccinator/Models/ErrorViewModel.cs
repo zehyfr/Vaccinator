d7f29dcb7c77bf19601576c49822bd2bf3864d9e
Baptiste-Boursin <baptiste.boursin@epsi.fr>
2021-04-03 11:38:14 +0200

    First commit

---
+++ Vaccinator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vaccinator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
